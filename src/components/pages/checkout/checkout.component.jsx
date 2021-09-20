7b2bfbbe8841c93dee10e5f2f3388c1782fe2b9a
fili34 <thgigi@hotmail.com>
2021-09-20 15:33:04 +0200

    added new favicon and title, updating stripe integration

--- src/components/pages/checkout/checkout.component.jsx
+++ src/components/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../../redux/cart/cart.selectors';
 import CheckoutItem from '../../checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../stripe-button/stripe-button.component';
 import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -29,6 +30,11 @@ const CheckoutPage = ({ cartItems, total }) => (
                <CheckoutItem key={cartItem.id} cartItem={cartItem}/> 
         ))}
         <div className='total'>TOTAL: ${total}</div>
+        <div className='test-warning'>
+         *Please use the following test credit card for payments* <br/>
+            4242 4242 4242 4242 - Exp: 01/22-CW:123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51JbkKwCCjGLewuCRf6zlezZMkQwoPbO3hWnVH3Kr25Y43C1WyUFwBLucFqTTcMhHRflOlgIAj14iyAk2AmdtHxr700WLOt6nuj'
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
